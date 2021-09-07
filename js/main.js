c90e9faff6d45ca26eb8d66a41523f171f624d1a
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-09-07 11:50:26 +0100

    header background color scroll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 // Main JS
+
+// Script to change background color header
+
+$(window).on("scroll", function() {
+if($(window).scrollTop() > 50) {
+  $("#main-header").addClass("bg-on");
+} else {
+  //remove the background property so it comes transparent again (defined in your css)
+ $("#main-header").removeClass("bg-on");
+}
+});
+
+// AOS Animate 
+
+AOS.init({
+duration : 1000, // duration of the animation
+once: true       // execute only once
+});
